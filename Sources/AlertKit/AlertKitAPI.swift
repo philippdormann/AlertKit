f68820a5ce6becc466d49a9bd2cb726ccb4ff1d3
Philipp Dormann <philipp@philippdormann.de>
2023-10-20 19:36:49 +0200

    wip

--- Sources/AlertKit/AlertKitAPI.swift
+++ Sources/AlertKit/AlertKitAPI.swift
@@ -7,11 +7,10 @@ public enum AlertKitAPI {
         view.present(on: window, completion: completion)
     }
     
-    public static func present(title: String? = nil, subtitle: String? = nil, icon: AlertIcon? = nil, haptic: AlertHaptic? = nil) -> some View {
+    public static func present(title: String? = nil, subtitle: String? = nil, icon: AlertIcon? = nil, haptic: AlertHaptic? = nil) {
         guard let window = UIApplication.shared.windows.filter({ $0.isKeyWindow }).first else { return }
         let view = AlertKitView(title: title, subtitle: subtitle, icon: icon)
         view.haptic = haptic
         view.present(on: window)
-        return view
     }
 }
